bb06098df006c7807f139e486530e5e4ca7f16b1
lnbt07 <61739374+lnbt07@users.noreply.github.com>
2020-03-03 09:45:05 -0300

    Rename Conta.java to Fontes-Aplicacao/Conta.java

--- Fontes-Aplicacao/Conta.java
+++ Fontes-Aplicacao/Conta.java
@@ -1,35 +1,35 @@
 
 public class Conta {
-	// atributos ou propriedades ou variáveis de instância
+	// atributos ou propriedades ou variÃ¡veis de instÃ¢ncia
 	int numero;
 	double saldo;
 	double limite;
 
-	// método para sacar um valor
+	// mÃ©todo para sacar um valor
 	public void sacar(double valor) {
 		saldo -= valor;
 	}
 	
-	//método para depositar um valor na conta
+	//mÃ©todo para depositar um valor na conta
 	public void depositar(double valor) {
 		saldo += valor;
 	}
 	
-	//método para retornar o saldo do cliente
+	//mÃ©todo para retornar o saldo do cliente
 	public double consultarSaldo() {
 		return saldo;
 	}
 	
-	//método para retornar os dados da conta
+	//mÃ©todo para retornar os dados da conta
 	public String retornarDados() {
 		String aux = "";
-		aux += "Número -> "+numero+"\n";
+		aux += "NÃºmero -> "+numero+"\n";
 		aux += "Saldo -> R$ "+saldo+"\n";
 		aux += "Limite -> R$ "+limite+"\n";
 		return aux;
 	}
 	
-	//método para transferir dinheiro para outra conta
+	//mÃ©todo para transferir dinheiro para outra conta
 	public void transferir(Conta destino, double valor) {
 		sacar(valor);
 		destino.depositar(valor);
